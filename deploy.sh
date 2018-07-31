65d6deb444e64c351eef3769dc7ab8fe2e0aa765
Jared Ririe <jaredririe20@gmail.com>
2018-07-31 13:03:12 -0600

    Restructure generated site.
    * Remove the generated static site in favor of storing it in
    a separate Git repo.
    * Write a script to deploy changes to the separate repo.
    * Explain where the hosted site can be found in the README.

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# Generate the static site in the default /public folder.
+hugo
+
+# Add changes to the git submodule.
+cd public
+git add .
+
+# Commit changes.
+msg="Rebuilding site on `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push changes to origin.
+git push origin master
+
+# Come back up to the project's root.
+cd ..
