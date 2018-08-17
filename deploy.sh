6146fc50d53d86f8fa433870a1cf56b3b0ad5601
Jared Ririe <jaredririe20@gmail.com>
2018-08-17 13:34:33 -0600

    Update deploy script to wipe contents of public before re-creating it.

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,15 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
+# Remove the contents of the current /public folder
+rm -rf ./public/*
+
 # Generate the static site in the default /public folder.
 hugo
 
 # Add changes to the git submodule.
 cd public
+git checkout README.md
 git add .
 
 # Commit changes.
